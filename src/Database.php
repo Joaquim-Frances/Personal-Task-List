a0eb48de9175f38b0567b04a6218ba36048ea46c
Quim79 <72190276+Quim79@users.noreply.github.com>
2020-11-06 16:47:32 +0100

    Added Task Model & Database Conection

--- src/Database.php
+++ src/Database.php
@@ -17,6 +17,21 @@ class database{
 
     }
 
-}
+    private function getConnection(){
 
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $database = "db_tasques_quim";
+        $charset = "utf-8";
+        $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+        $pdo = new PDO("mysql:host={$host}; dbname={$database}; charset={$charset}", $user, $pass, $options);
+        $pdo->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }
+}
 ?>
+
+
+
--- src/Model/Task.php
+++ src/Model/Task.php
@@ -1,21 +1,37 @@
 <?php
 
+use App\Database;
+
+
 class Task{
 
     public  $id;
     public  $taskName;
     public  $dateCreation;
+    public  $descripcio;
 
-
-    public function __construct($id = null, $taskName = "", $dateCreation = null){
+    public function __construct($id = null, $taskName = "", $dateCreation = null, $descripcio =""){
 
         $this->id = $id;
         $this->name = $name;
         $this->dateCreation = $dateCreation;
+        $this->descripcio = $descripcio;
 
         if(!$this->database){
             $this->database = new database;
         }
+    }
+
+    public function listTasks(){
+
+        $allTasks = $this->database->mysql->query("SELECT * FROM tasques");
+        $taskList = [];
+        foreach ( $allTasks as $task){
+            $fullTask = new Task ($task["id"], $task["name"], $task["fecha"], $task["descripcio"]);
+            array_push($taskList, $fullTask);
+        }
+
+        return $taskList;
     }
 
 }
